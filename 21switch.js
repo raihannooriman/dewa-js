53e6eefc68b300b5bf270fe2cc2fb923e72fb53d
Raihan Nooriman <noorimanr@gmail.com>
2025-01-12 18:27:09 +0800

    x

--- 21switch.js
+++ 21switch.js
@@ -1,16 +1,17 @@
-let value = "C";
+let value = "B";
 switch (value) {
   case "A":
     console.log("A");
     break;
   case "B":
+    console.log("B");
   case "C":
     console.log("BC");
     break;
   default:
     console.log("default");
 }
-// output: A
+// output: B, BC (tidak ada break pada case "B")
 // penjelasan
 // value = "C" maka akan mengeksekusi case "C" dan break, sehingga outputnya adalah "C"
 // break digunakan untuk menghentikan eksekusi kode dalam switch case
