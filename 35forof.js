b5dd8c20539e82d12c15557bb3da2ac4734763bc
Raihan Nooriman <noorimanr@gmail.com>
2025-01-19 00:36:54 +0800

    dc

--- 35forof.js
+++ 35forof.js
@@ -2,9 +2,10 @@ let names = ["John", "Doe"];
 for (const name of names) {
   console.log(name);
 }
-// output: John
+// output: John, Doe
 let fullname = "John Doe";
 for (const character of fullname) {
   console.log(character);
 }
 // output: J o h n   D o e
+// mengapa jadi terpisah per karakter? karena string di javascript adalah array of character (array of string) jika diakses per karakter tanpa index, jika diakses per kata maka harus menggunakan index array.
